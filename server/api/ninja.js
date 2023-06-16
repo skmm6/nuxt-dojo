46c16ab6c6dffe10febe39a0e6adfda6f0ff3b41
skmm6 <62322231+skmm6@users.noreply.github.com>
2023-06-16 11:34:38 +0300

    Update ninja.js

--- server/api/ninja.js
+++ server/api/ninja.js
@@ -1,16 +1,19 @@
 export default defineEventHandler(async (event) => {
-  //   // handle query params
-  //   const { name } = getQuery(event);
 
-  //   //   handle post data
-  //   const { age } = await readBody(event);
+  // handle query params
+  // const { name } = useQuery(event)
 
-  //   api call whitch private key
-  const { currencyKey } = useRuntimeConfig();
+  // handle post data
+  // const { age } = await useBody(event)
 
-  const { data } = await $fetch(
-    `https://api.currencyapi.com/v3/latest?apikey=${currencyKey}`
-  );
+  // return {
+  //   message: `Hello, ${name}! You are ${age} years old.`
+  // }
 
-  return data;
-});
+  // api call with private key
+  const { currencyKey } = useRuntimeConfig()
+
+  const { data } = await $fetch(`https://api.currencyapi.com/v3/latest?apikey=${currencyKey}`)
+
+  return data
+})
